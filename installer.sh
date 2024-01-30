abeadf2a495318692c34de36fb1ff31dd28981bf
MarcusM2092 <113401960+MarcusM2092@users.noreply.github.com>
2024-01-31 10:31:35 +1100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,7 @@
 #!bin/bash
+
 sudo apt update
 sudo apt upgrade -y
-sudo apt install wget curl python python3 python3-pip -y
+sudo apt install wget curl python python3 pip -y
 curl -sSL https://install.pi-hole.net | bash
 sudo pip install pihole5-list-tool --upgrade
